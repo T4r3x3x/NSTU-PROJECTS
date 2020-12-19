33ba8cf9a63e7f7f2b80cb393eac605f499f291b
T4r3x3x <73325629+T4r3x3x@users.noreply.github.com>
2020-12-19 11:24:31 +0700

    Update Code.cs

--- PZ3/Code.cs
+++ PZ3/Code.cs
@@ -1,296 +1,245 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PZ3_4.New;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace PZ3
+namespace UnitTest
 {
-    class Program
+    [TestClass]
+    public class UnitTest1
     {
-        static void Main(string[] args)
+        Variable x = new Variable("x");
+        Variable y = new Variable("y");
+        Expr expr;
+        double expected;
+        double actual;
+
+        [TestMethod]
+        public void ConstantSumConstant()
         {
-            Variable x = new Variable("x");
-            Variable y = new Variable("y");
+            expr = new Constant(-1) + new Constant(11);
+            expected = 10;
+            Assert.IsTrue(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(null);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ConstantSubConstant()
+        {
+            expr = new Constant(-32.25) - new Constant(15.15);
+            expected = -47.4;
+            Assert.IsTrue(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(null);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ConstantMulConstant()
+        {
+            expr = new Constant(0.5) * new Constant(1000);
+            expected = 500;
+            Assert.IsTrue(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(null);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ConstantDivConstant()
+        {
+            expr = new Constant(32.0) / new Constant(64.0);
+            expected = 0.5;
+            Assert.IsTrue(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(null);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void VariableSumVariable()
+        {
+            expr = x + y;
+            expected = 8.15;
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = -2, ["y"] = 10.15 });
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void VariableSubVariable()
+        {
+            expr = x - y;
+            expected = -18;
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = 14.15, ["y"] = 32.15 });
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void VariableMulVariable()
+        {
+            expr = x * y;
+            expected = 24;
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = -3.2, ["y"] = -7.5 });
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void VariableDivVariable()
+        {
+            expr = x / y;
+            expected = 1.01;
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = 16.16, ["y"] = 16 });
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void VariableDivConstant()
+        {
+            expr = x / new Constant(2.0);
+            expected = 2;
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = 4 });
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ConstantDivVariable()
+        {
+            expr = new Constant(1) / x;
+            expected = 0.008;
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = 125 });
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InvertVariable()
+        {
+            expr = new Minus(x);
+            expected = -1.0;
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = 1.0 });
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void InvertInvertVariable()
+        {
+            expr = new Minus(new Minus(x));
+            expected = 1.0;
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsTrue(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = 1.0 });
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FunctionSin()
+        {
+            expr = new Sin(x);
+            expected = Math.Sin(Math.PI / 4);
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsFalse(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = Math.PI / 4 });
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FunctionCos()
+        {
+            expr = new Cos(x);
+            expected = Math.Cos(Math.PI / 8);
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsFalse(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = Math.PI / 8 });
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FunctionTg()
+        {
+            expr = new Tg(x);
+            expected = Math.Tan(Math.PI / 3);
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsFalse(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = Math.PI / 3 });
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FunctionCtg()
+        {
+            expr = new Ctg(x);
+            expected = 1 / Math.Tan(Math.PI / 13);
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsFalse(expr.IsPolynom);
+            actual = expr.Compute(new Dictionary<string, double> { ["x"] = Math.PI / 13 });
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void VariableException()
+        {
+            expr = x;
+            actual = x.Compute(new Dictionary<string, double> { });
+            expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ExprToString()
+        {
             Variable z = new Variable("z");
-            Variable c = new Variable("c");
-            Constant alpha = new Constant(3);
-            foreach (var item in (x * y * z / x - z - c).Variables)
-            {
-                Console.WriteLine(item);
-            }
-          //  Vector vector = new Vector(new Expr[3] { x,y,z });
-          //  Console.WriteLine((vector - vector).Compute(new Dictionary<string, double> { ["x"] = 5, ["y"] = 3 , ["z"] = 1}));
-
-           
-        }
-    }
-
-    public interface IExpr
-    {
-        double Compute(IReadOnlyDictionary<string, double> variableValues);
-
-        IEnumerable<string> Variables { get; }
-        bool IsConstant { get; }
-        bool IsPolynom { get; }
-    }
-    abstract class Expr : IExpr
-    {
-        public abstract double Compute(IReadOnlyDictionary<string, double> variableValues);
-
-        public virtual IEnumerable<string> Variables { get; }
-        public abstract bool IsConstant { get; }
-        public abstract bool IsPolynom { get; }
-
-        public static Add operator +(Expr a, Expr b) => new Add(a, b);
-        public static Sub operator -(Expr a, Expr b) => new Sub(a, b);
-        public static Mult operator *(Expr a, Expr b) => new Mult(a, b);
-        public static Divide operator /(Expr a, Expr b) => new Divide(a, b);
-    }
-
-    #region Binary Operations
-    abstract class BinaryOperation : Expr
-    {
-        public BinaryOperation(Expr arg1, Expr arg2)
-        {
-            Arg1 = arg1;
-            Arg2 = arg2;
-        }
-        public override bool IsConstant => Arg1.IsConstant && Arg2.IsConstant;
-        public override bool IsPolynom => Arg1.IsPolynom && Arg2.IsPolynom;
-        public override IEnumerable<string> Variables => Arg2.Variables != null ? Arg1.Variables.Union(Arg2.Variables) : Arg1.Variables;
-        public Expr Arg1 { get; }
-        public Expr Arg2 { get; }
-    }
-
-
-    class Add : BinaryOperation
-    {
-        public Add(Expr a, Expr b) : base(a, b) { }
-
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues) =>
-            Arg1.Compute(variableValues) + Arg2.Compute(variableValues);
-
-        public override string ToString() => $"{Arg1} + {Arg2}";
-
-    }
-    class Sub : BinaryOperation
-    {
-        public Sub(Expr a, Expr b) : base(a, b) { }
-
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues) =>
-            Arg1.Compute(variableValues) - Arg2.Compute(variableValues);
-
-
-        public override string ToString() => $"{Arg1} - " + (Arg2.GetType().IsSubclassOf(typeof(BinaryOperation)) ? $"({Arg2})" : $"{Arg2}");
-    }
-
-    class Mult : BinaryOperation
-    {
-        public Mult(Expr a, Expr b) : base(a, b) { }
-
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues) =>
-            Arg1.Compute(variableValues) * Arg2.Compute(variableValues);
-
-
-        public override string ToString() => $"{Arg1} * {Arg2}";
-    }
-
-    class Divide : BinaryOperation
-    {
-        public Divide(Expr a, Expr b) : base(a, b) { }
-
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues) =>
-            Arg1.Compute(variableValues) / Arg2.Compute(variableValues);
-
-        public override string ToString() => $"{Arg1} / {Arg2}";
-    }
-    #endregion
-
-    class Variable : Expr
-    {
-        public string Value
-        {
-            get;
-        }
-        public override IEnumerable<string> Variables { get; }
-
-        public Variable(string value)
-        {
-            Value = value;
-            Variables = new string[1] { value };
+            Variable t = new Variable("t");
+            expr = (x+z)/(t-(t+y)*y)-(new Constant(4)-new Constant(2.5));
+            string expected = "(x + z) / (t - (t + y) * y) - (4 - 2,5)";
+            Assert.AreEqual(expected, expr.ToString());
         }
 
-        public override bool IsConstant => false;
-        public override bool IsPolynom => true;
-
-
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues)
+        [TestMethod]
+        public void VarVariablesProperty()
         {
-            double value;
-            variableValues.TryGetValue(Value, out value);
-            return value;
+            Variable z = new Variable("z");
+            Variable t = new Variable("t");
+            expr = (x + z) / (t - (t + y) * y) - (x - z);
+            char[] expected = { 'x', 'z', 't', 'y' };
+            char[] actual = expr.Variables.ToString().ToCharArray();
+            Assert.AreEqual(expected.ToString(), actual.ToString());
         }
-        public override string ToString() => Value;
-    }
-
-    class Constant : Expr
-    {
-        public double Value { get; }
-        public Constant(double value)
+        [TestMethod]
+        public void ConstVariablesProperty()
         {
-            Value = value;
+           Constant five = new Constant(5);
+            Constant four = new Constant(4);
+            expr = five + four;
+            IEnumerable<string> actual = expr.Variables;
+            IEnumerable<string> expected = null;
+            Assert.AreEqual(expected, actual);
         }
-
-        public override bool IsConstant => true;
-        public override bool IsPolynom => true;
-
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues) => Value;
-        public override string ToString() => Value.ToString();
-    }
-
-    abstract class UnaryOperation : Expr
-    {
-        public UnaryOperation(Expr arg)
+        [TestMethod]
+        public void ConstVarVariablesProperty()
         {
-            Arg = arg;
+            Constant five = new Constant(5);
+            Constant four = new Constant(4);
+            expr = (five + four )+ (x + y);
+            char[] actual = expr.Variables.ToString().ToCharArray();
+            char[] expected = { 'x', 'y' };
+            Assert.AreEqual(expected.ToString(), actual.ToString());
         }
-        public override bool IsConstant => Arg.IsConstant;
-        public override bool IsPolynom => Arg.IsPolynom;
-        public override IEnumerable<string> Variables => Arg.Variables;
-        public Expr Arg { get; }
-    }
-
-    class Minus : UnaryOperation
-    {
-        public Minus(Expr a) : base(a) { }
-
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues)
+        [TestMethod]
+        public void TrigToString()
         {
-            return -Arg.Compute(variableValues);
-        }
-
-        public override string ToString()
-        {
-            return Arg.GetType().IsSubclassOf(typeof(BinaryOperation)) ? $"-({Arg})" : $"-{Arg}";
-        }
-    }
-
-    class Diff : UnaryOperation
-    {
-        public Diff(Expr arg) : base(arg) { }
-
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    abstract class Function : UnaryOperation
-    {
-        public Function(Expr a) : base(a) { }
-        public override bool IsPolynom => false;
-    }
-    class Cos : Function
-    {
-        public Cos(Expr a) : base(a) { }
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues) => Math.Cos(Arg.Compute(variableValues));
-        public override string ToString() => $"cos({Arg})";
-    }
-    class Sin : Function
-    {
-        public Sin(Expr a) : base(a) { }
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues) => Math.Sin(Arg.Compute(variableValues));
-
-        public override string ToString() => $"sin({Arg})";
-    }
-    class Tg : Function
-    {
-        public Tg(Expr a) : base(a) { }
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues) => Math.Tan(Arg.Compute(variableValues));
-
-        public override string ToString() => $"tg({Arg})";
-    }
-    class Ctg : Function
-    {
-        public Ctg(Expr a) : base(a) { }
-        public override double Compute(IReadOnlyDictionary<string, double> variableValues) => (1 / Math.Tan(Arg.Compute(variableValues)));
-
-        public override string ToString() => $"ctg({Arg})";
-    }
-
-    abstract class VExpr //написать скалярное и векторное произведение + реверс знаков
-    {
-        public abstract Expr[] Values { get; }
-
-        public abstract Vector Compute(IReadOnlyDictionary<string, double> variableValues);
-
-        public static Vector operator +(VExpr a, VExpr b)
-        {
-            Expr[] result = new Expr[a.Values.Length];
-            for (int i = 0; i < a.Values.Length; i++)
-            {
-                result[i] = a.Values[i] + b.Values[i];
-            }
-            return new Vector(result);
-        }
-        public static Vector operator -(VExpr a, VExpr b)
-        {
-            Expr[] result = new Expr[a.Values.Length];
-            for (int i = 0; i < a.Values.Length; i++)
-            {
-                result[i] = a.Values[i] - b.Values[i];
-            }
-            return new Vector(result);
-        }
-        public static Vector operator *(VExpr a, VExpr b)
-        {
-            Expr[] result = new Expr[a.Values.Length];
-            for (int i = 0; i < a.Values.Length; i++)
-            {
-                result[i] = a.Values[i] * b.Values[i];
-            }
-            return new Vector(result);
-        }
-    }
-
-    class Vector : VExpr 
-    {
-        Expr[] values;
-        public override Expr[] Values => values;
-        public Vector(Expr[] values) 
-        {
-            this.values = values;
-        }
-
-        public override Vector Compute(IReadOnlyDictionary<string, double> variableValues)
-        {
-            Constant[] result = new Constant[values.Length];
-            for (int i = 0; i < values.Length; i++)
-            {
-                result[i] =new Constant(values[i].Compute(variableValues));
-            }
-            return new Vector(result);
-        }
-        public Expr Norm() //нет корня
-        {
-            Expr result = values[0] * values[0];
-
-            for (int i = 1; i < values.Length; i++)
-            {
-                result += values[i] * values[i];
-            }
-
-            return result;
-        }
-
-        public override string ToString()
-        {
-            string result = "(";
-            for (int i = 0; i < values.Length-1; i++)
-            {
-                result += $"{values[i]}, ";
-            }
-            result+=$"{values[values.Length-1]})";
-            return result;
+            Variable z = new Variable("z");
+            Variable t = new Variable("t");
+            expr = new Sin(x/y) * new Cos(y+x) - new Tg(z-y) / new Ctg(t*x);
+            string expected = "sin(x / y) * cos(y + x) - tg(z - y) / ctg(t * x)";
+            string actual = expr.ToString();
+            Assert.IsFalse(expr.IsConstant);
+            Assert.IsFalse(expr.IsPolynom);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
