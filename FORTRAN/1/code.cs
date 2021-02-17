9e5fe42a6d2bf1115562e992795368293ec0b3ca
T4r3x3x <73325629+T4r3x3x@users.noreply.github.com>
2021-02-17 17:59:22 +0700

    Update code.cs

--- FORTRAN/1/code.cs
+++ FORTRAN/1/code.cs
@@ -68,6 +68,12 @@
                             end if
                         end if               
                     end if
+                else
+                    if(a .EQ. 0 .AND. b .EQ.. 0 .AND. c .EQ. 0) then    
+                        return
+                    else
+                        isExist = .true.   
+                    end if
                 end if
                 return 
                 end
@@ -75,8 +81,12 @@
                 function Square()
                 common /triangle/ a,b,c
                 real Square, p 
-                p = simePerimetr()
-                Square = sqrt(p*(p-a)*(p-b)*(p-c))
+                if(a .NE. 0 .AND. b .NE. 0 .AND. c .NE. 0) then
+                    p = simePerimetr()
+                    Square = sqrt(p*(p-a)*(p-b)*(p-c))
+                else
+                    Square = 0
+                end if
                 return 
                 end
                 
@@ -90,14 +100,18 @@
                 function min_angle()  
                 common /triangle/ a,b,c     
                 real min_angle
-                if(a .LE. b .AND. a .LE. c) then
-                    min_angle = acos(get_cos(a,b,c))
-                else 
-                    if(b .LE. c .AND. b .LE. c) then
-                        min_angle = acos(get_cos(b,c,a)) 
+                if(a .NE. 0 .AND. b .NE. 0 .AND. c .NE. 0) then                                    
+                    if(a .LE. b .AND. a .LE. c) then
+                        min_angle = acos(get_cos(a,b,c))
                     else 
-                        min_angle = acos(get_cos(c,a,b)) 
-                    end if               
+                        if(b .LE. c .AND. b .LE. c) then
+                            min_angle = acos(get_cos(b,c,a)) 
+                        else 
+                            min_angle = acos(get_cos(c,a,b)) 
+                        end if               
+                    end if    
+                else
+                    min_angle = 0    
                 end if    
                 min_angle = min_angle*180/3.1416
                 return                            
@@ -107,14 +121,18 @@
                 common /triangle/ a,b,c     
                 real min_cos
                 min_cos =0
-                if(a .GE. b .AND. a .GE. c) then
-                    min_cos = get_cos(a,b,c)
-                else 
-                    if(b .GE. c .AND. b .GE. c) then
-                        min_cos = get_cos(b,c,a)
+                if(a .NE. 0 .AND. b .NE. 0 .AND. c .NE. 0) then     
+                    if(a .GE. b .AND. a .GE. c) then
+                        min_cos = get_cos(a,b,c)
                     else 
-                        min_cos = get_cos(c,a,b)
+                        if(b .GE. c .AND. b .GE. c) then
+                            min_cos = get_cos(b,c,a)
+                        else 
+                            min_cos = get_cos(c,a,b)
+                        end if
                     end if               
+                else
+                    min_cos = 1        
                 end if    
                 return 
                 end 
