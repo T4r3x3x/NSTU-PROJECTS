9fb076be6c862fa83d8e44c790b207ae6ce15c2f
T4r3x3x <73325629+T4r3x3x@users.noreply.github.com>
2021-02-25 13:19:39 +0700

    Update Code.cs

--- FORTRAN/2/Code.cs
+++ FORTRAN/2/Code.cs
@@ -1,9 +1,11 @@
         program main
         implicit none 
-        real min_x, max_x, step_x, min_y, max_y, step_y, Func,i, j
+        real Pi
+        parameter(Pi = 3.1415926535)         
+        real min_x, max_x, step_x, min_y, max_y, step_y, i, j
         integer steps_x, steps_y
         
-        OPEN (1,FILE='C:\WATCOM\Output.txt') 
+        OPEN (1,FILE='C:\Users\pmi-b9205\Desktop\Output.txt') 
         
         print *, 'Enter min x'
         read *, min_x
@@ -20,24 +22,28 @@
         
         steps_x = (max_x - min_x) / step_x
         steps_y = (max_y - min_y) / step_y
-        write(1,'(A$)') '           '
+        write(1,'(A$)') '       | '
         do i = min_x, max_x, step_x
-            write(1,'(f11.5$)') i
+            write(1,'(f6.4$, (A$))') i, ' | '
         end do 
         write(1,*) ' '
         do i = min_y, max_y, step_y
-            write(1,'(f11.5$)') i
+            write(1,'(f6.4$,(A$))') i, ' | '
             do j = min_x, max_x, step_x
-                write (1, '(f11.5$)') Func(i,j)     
+                call Func(j,i)     
             end do
             write (1,*) ' '
         end do
-        read *, i
         close (1)
         end
         
         
-        function Func(x, y)    
-            Func = abs(cos(x)/sin(y))
+        subroutine Func(x, y)   
+            real x,y 
+            if(sin(y) .NE. 0) then
+                write (1, '(f6.4$, A$)') abs(cos(x)/sin(y)),' | ' 
+            else
+                write(1, '(A$)') 'div. 0 | '
+            end if             
             return
         end
