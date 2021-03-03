d8bcc4b8973b4fcb10561160f1f5ebdc66ec8127
T4r3x3x <73325629+T4r3x3x@users.noreply.github.com>
2021-03-03 19:24:08 +0700

    Update Code.cs

--- FORTRAN/2/Code.cs
+++ FORTRAN/2/Code.cs
@@ -1,49 +1,55 @@
         program main
         implicit none 
         real Pi
-        parameter(Pi = 3.1415926535)         
-        real min_x, max_x, step_x, min_y, max_y, step_y, i, j
+        parameter(pi = 3.1415926535)      
+        real min_x, max_x, step_x, min_y, max_y, step_y, i, j,k
         integer steps_x, steps_y
+        OPEN (1,FILE='C:\Users\hardb\Desktop\Input.txt')
+        OPEN (2,FILE='C:\Users\hardb\Desktop\Output.txt') 
         
-        OPEN (1,FILE='C:\Users\pmi-b9205\Desktop\Output.txt') 
+        read (1,*) min_x
+        read (1,*) max_x
+        read (1,*) step_x
+        read (1,*) min_y
+        read (1,*) max_y
+        read (1,*) step_y
+       
+        if(max_x .LT. min_x) then
+            write(2,*) 'x incorrent range'
+            stop
+        else if(max_y .LT. min_y) then
+            write(2,*) 'y incorrent range'
+            
+        else if(step_x .EQ. 0) then
+            write(2,*) 'step x incorrent range'
+            stop
+        else if(step_y .EQ. 0) then
+            write(2,*) 'step x incorrent range' 
+            stop
+        else
+            write(2,'(A$)') '             | '
+            do k = min_x, max_x, step_x
+                write(2,'(e12.4$, (A$))') k, ' | '
+            end do 
+            write(2,*) ' '
         
-        print *, 'Enter min x'
-        read *, min_x
-        print *, 'Enter max x'
-        read *, max_x
-        print *, 'Enter step x'
-        read *, step_x
-        print *, 'Enter min y'
-        read *, min_y
-        print *, 'Enter max y'
-        read *, max_y
-        print *, 'Enter step y'
-        read *, step_y
-        
-        steps_x = (max_x - min_x) / step_x
-        steps_y = (max_y - min_y) / step_y
-        write(1,'(A$)') '       | '
-        do i = min_x, max_x, step_x
-            write(1,'(f6.4$, (A$))') i, ' | '
-        end do 
-        write(1,*) ' '
-        do i = min_y, max_y, step_y
-            write(1,'(f6.4$,(A$))') i, ' | '
-            do j = min_x, max_x, step_x
-                call Func(j,i)     
+            do i = min_y, max_y, step_y
+                write(2,'(e12.4$,(A$))') i, ' | '
+                do j = min_x, max_x, step_x
+                    call Func(j*pi/180.0,i*pi/180.0)     
+                end do
+                write (2,*) ' '
             end do
-            write (1,*) ' '
-        end do
-        close (1)
-        end
+       end if 
+       end
         
         
-        subroutine Func(x, y)   
+        subroutine Func(x, y)             
             real x,y 
             if(sin(y) .NE. 0) then
-                write (1, '(f6.4$, A$)') abs(cos(x)/sin(y)),' | ' 
+                write (2, '(e12.4$, A$)') abs(cos(x)/sin(y)),' | ' 
             else
-                write(1, '(A$)') 'div. 0 | '
+                write(2, '(A$)') '  div. by 0  | '
             end if             
             return
         end
