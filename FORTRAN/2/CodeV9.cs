0894fcad773f23ef1bc7ee8077a143b23bc535e0
T4r3x3x <73325629+T4r3x3x@users.noreply.github.com>
2021-04-22 00:45:15 +0700

    Update CodeV9.cs

--- FORTRAN/2/CodeV9.cs
+++ FORTRAN/2/CodeV9.cs
@@ -1,11 +1,10 @@
-        program main
-        
+      program main
+                
         real min_x, max_x, step_x, min_y, max_y, step_y, i, j,x,y
-        real temp
         integer steps_x, steps_y
-        logical invisible 
-        OPEN (1,FILE='C:\Users\pmi-b9205\Desktop\Input.txt')
-        OPEN (2,FILE='C:\Users\pmi-b9205\Desktop\Output.txt') 
+        logical IsSame 
+        OPEN (1,FILE='C:\Users\hardb\Desktop\Input.txt')
+        OPEN (2,FILE='C:\Users\hardb\Desktop\Output1.txt') 
         
         read (1,*) min_x 
         read (1,*) max_x
@@ -13,18 +12,21 @@
         read (1,*) min_y
         read (1,*) max_y
         read (1,*) step_y
+        
+        x = -min_x-1
+        y = -min_y-1
                 
         steps_x = (max_x-min_x)/step_x
-        steps_y = (max_y-min_y)/step_y
-        
+        steps_y = (max_y-min_y)/step_y       
         write(2,'(A$)') '             | '
         
-         do i = 0, steps_x            
-            if(invisible(x,sum_rump(min_x,i*step_x))) then    
-                x = sum_rump(min_x,i*step_x)          
-                write(2,'(e12.4$,(A$))') x, ' | '                  
-            end if    
-            x = sum_rump(min_x,i*step_x)               
+        do i = 0, steps_x            
+           if(IsSame(x,min_x + i*step_x)) then    
+               x = min_x + i*step_x      
+               write(2,'(e12.4$,(A$))') x, ' | '                  
+           else   
+               write(2,'(A$)') '             | ' 
+           end if              
         end do
         
         if(x .LT. max_x) then
@@ -34,91 +36,81 @@
         write (2,*) ' '
         
         do i = 0, steps_y
-             if(invisible(y,sum_rump(min_y,i*step_y))) then 
-                y = sum_rump(min_y,i*step_y)
-                write(2,'(e12.4$,(A$))') y, ' | '
-             end if
-            y = sum_rump(min_y,i*step_y)
+            if(IsSame(y,min_y+i*step_y)) then 
+               y = min_y + i*step_y
+               write(2,'(e12.4$,(A$))') y, ' | '          
             
+            x = -min_x-1
             do j = 0, steps_x
-                if(invisible(x,sum_rump(min_x,i*step_x))) then 
-                    x = sum_rump(min_x,j*step_x)                
+                if(IsSame(x,min_x + j*step_x)) then 
+                    x = min_x + j*step_x                
                     call func(x,y)              
+                else
+                    write(2,'(A$)') '             | '
                 end if
-                x = sum_rump(min_x,j*step_x)
             end do 
             
             if(x .LT. max_x) then
                 call Func(max_x,y)
             end if
             
-            write (2,*) ' '                            
+            write (2,*) ' ' 
+            
+            else
+            write(2,'(A$)') '             | '
+            end if
+                                   
         end do
+        
+        
         
         if(y .LT. max_y) then
                     write(2,'(e12.4$,(A$))') max_y, ' | '
                     y = max_y
+                    x = -min_x-1
                     do j = 0, steps_x
-                        if(invisible(x,sum_rump(min_x,i*step_x))) then 
-                            x = sum_rump(min_x,j*step_x)                
+                        if(IsSame(x,min_x + j*step_x)) then 
+                            x = min_x + j*step_x                
                             call func(x,y)              
+                        else 
+                            write(2,'(A$)') '             | '                       
                         end if
-                        x = sum_rump(min_x,j*step_x)                       
                     end do 
             
                     if(x .LT. max_x) then
                         call Func(max_x,y)
                     end if
-            end if            
+            end if  
         end 
-       
-       
-       
-        REAL FUNCTION sum_rump(X)
-            real x,s,c,e, twosum
-            s = 0.0
-            c = 0.0
-            s = twosum(x,s,e)
-            sump_rump = s + c
-            return
-        end       
-       
-        REAL FUNCTION TWOSUM(ERROR, A, B)  
-      
-        IMPLICIT NONE
-        REAL A, B, ERROR
-        REAL s
-        s = a + b
-       
-      
-        END
         
-        
-        subroutine Func(x, y)             
-            real x,y 
-            if(cos(y) .NE. 0) then
-                write (2, '(e12.4$, A$)') abs(sin(x)/cos(y)),' | ' 
+        subroutine Func(x, y) 
+            implicit none  
+            real x,y,yt,xt,Pi
+            parameter(pi = 3.1415926535)
+            call Around(y,y) 
+            if (y .EQ. 90.0) then
+                write(2, '(A$)') '  div. by 0  | '      
             else
-                write(2, '(A$)') '  div. by 0  | '
+            call around(x*PI/180.0,xt)
+            call around(y*PI/180.0,yt)
+      write(2,'(e12.4$, A$)')abs(sin(xt)/cos(yt)),' | '                 
             end if             
             return
         end
         
                
                   
-                 logical function Invisible(x,y)
+                 logical function IsSame(x,y)
                  implicit none
                  real x,y
-                 integer a,b
-                 call exp (x,a)
-                 call exp (y,b)
-                 print *, a
-                 print *, b
-                 if(a-b .EQ. 0) then
-                    Invisible = .FALSE.
+                 call Around(x,x)
+                 call Around(y,y)
+
+                 if(x .EQ. y) then
+                    IsSame = .FALSE.
                     return
                  else
-                    Invisible = .TRUE.                    
+                    IsSame = .TRUE.             
                     return
                   end if
                   end
@@ -126,63 +118,10 @@
                 
                 
                 
-                subroutine Exp(x, res)
+                subroutine Around(x,result)
                 implicit none
-                real x, k, temp
-                integer i, j, res
-                temp = x
-                j = 0
-                k = 10
-                
-                if( x .EQ. 1) then 
-                j = 3
-                else if( x .EQ. 10) then  
-                j = 2 
-                else if( x .EQ. 100) then 
-                j = 1 
-                else 
-                
-                
-                if(x .GE. 10000) then 
-                    j = x/10000
-                    k = 0.1
-                    if(x/10000 .NE. 0) then
-                        j = j + 1
-                    end if
-                else if (x .LT. 1000) then
-                    j = 1000/x   
-                    k = 10
-                end if
-                
-                call Times(j)
-                
-                end if 
-                
-                
-                do i = 1, j
-                    temp = temp * k
-                end do  
-                res = int(temp)
-                end
-                
-                
-                subroutine Times(j)
-                implicit none
-                integer i, k, j                
-                k = j
-                if(j .NE. 0) then
-                j = 1
-                else 
-                j=0
-                end if
-                
-                do i = 1, 10
-                    if(k/10 .EQ. 0) then 
-                        return
-                    else
-                        k = k / 10
-                        j = j +1
-                    end if
-                end do
-                
+                real x, result 
+                character*12 line
+                write(line,'(e12.4)') x
+                read(line,'(e12.4)')result           
                 end
